66ef42fea97f17d70923e8b6b10d73b888b33a50
konexio <damir.sagadbekov@live.fr>
2021-01-27 10:45:04 +0100

    water

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import './styles/global.css';
 import Box from "./components/Box"
 
+const waterMin = 1.5;
+const waterMax = 6;
 const tempMin = -20;
 const tempMax = 40;
 const heartMin = 80;
@@ -11,15 +13,32 @@ const stepsMin = 0;
 const stepsMax = 50000;
 
 export class App extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       water : 0,
       heart : 120,
       temperature : -10,
-      steps : 3000
+      steps : 3000,
+      onHeartChange : (value) => {
+        this.setState({heart:value.target.value});
+        this.state.calculateWater();
+      },
+      onStepsChange : (value) => {
+        this.setState({steps:value.target.value});
+        this.state.calculateWater();
+      },
+      onTempChange : (value) => {
+        this.setState({temperature:value.target.value});
+        this.state.calculateWater();
+      },
+      calculateWater : () => {
+        return this.setState({water:this.state.heart})
+      }
     }
   }
+  
+
   render() {
     return (
       <div class="container-fluid">
@@ -28,13 +47,13 @@ export class App extends React.Component {
           <p>Temperature : {tempMin}</p>
           <p>Steps : {stepsMin}</p>
           {/* Water */}
-          <Box icon={"local_drink"} color={"#3A85FF"} value={"1.5"} unit={"L"}/>
+          <Box icon={"local_drink"} color={"#3A85FF"} min={waterMin} max={waterMax} value={this.state.water} unit={"L"} onChange={this.state.calculateWater}/>
           {/* Steps */}
-          <Box icon={"directions_walk"} color={"black"} value={"3000"} unit={"steps"}/>
+          <Box icon={"directions_walk"} color={"black"} min={stepsMin} max={stepsMax} value={this.state.steps} unit={"steps"} onChange={this.state.onStepsChange}/>
           {/* Heart */}
-          <Box icon={"favorite"} color={"red"} value={"120"} unit={"bpm"}/>
+          <Box icon={"favorite"} color={"red"} min={heartMin} max={heartMax} value={this.state.heart} unit={"bpm"} onChange={this.state.onHeartChange}/>
           {/* Temperature */}
-          <Box icon={"wb_sunny"} color={"yellow"} value={"-10"} unit={"°C"}/>
+          <Box icon={"wb_sunny"} color={"yellow"} min={tempMin} max={tempMax} value={this.state.temperature} unit={"°C"} onChange={this.state.onTempChange}/>
         </div>
       </div>
     );
--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -1,25 +1,32 @@
 import React from "react";
-import InputRange from 'react-input-range';
 
 export class Box extends React.Component {
     render() {
-        return (
-            <div class="box col-sm-3 col-6">
-                <span style={{fontSize : 100, color : this.props.color}} class="material-icons">
-                    {this.props.icon}
-                </span>
-                <p>{this.props.value}{this.props.unit}</p>
-                if ( icon != "local_drink") {
-                    <InputRange 
-                     min = {this.props.min}
-                     max = {this.props}
-                     value = {this.props}
-                     oninput = {this.props} />
-                }
-            </div>
-        )
+        if (this.props.icon !== "local_drink" ) {
+            return (
+                <div class="box col-sm-3 col-6">
+                    <span style={{fontSize : 100, color : this.props.color}} class="material-icons">
+                        {this.props.icon}
+                    </span>
+                    <p>{this.props.value}{this.props.unit}</p>
+                    <input type="range" min={this.props.min} max={this.props.max} value={this.props.value} onChange={this.props.onChange}/> 
+                </div>   
+            ) 
+        }
+        else {
+            return (
+                <div class="box col-sm-3 col-6">
+                    <span style={{fontSize : 100, color : this.props.color}} class="material-icons">
+                        {this.props.icon}
+                    </span>
+                    <p>{this.props.value}{this.props.unit}</p>
+                </div>   
+            )
+        }
+    }           
+}        
         
-    }
-}
+    
+
 
 export default Box;
